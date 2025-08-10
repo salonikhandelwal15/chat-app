d163b2e7be98735312b367252459df9c305f5cee
Saloni <salonikhandelwal22553@gmail.com>
2025-08-10 18:27:53 +0530

    Add login page and profile page with basic UI and navigation

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -9,16 +9,27 @@ const LoginPage = () => {
   const[bio,setBio]=useState("")
   const[isDataSubmitted,setIsDataSubmitted]=useState(false)
 
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+
+    if(currState === 'Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
+
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
       {/*------left-------*/}
       <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
       {/*------right-------*/}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+      <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
       <h2 className='font-medium text-2xl flex justify-between items-center'>
         {currState}
-        <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
-        </h2>{currState ==="Sign up" && !isDataSubmitted && (
+        {isDataSubmitted && <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>}
+        
+        </h2>
+        {currState ==="Sign up" && !isDataSubmitted && (
           <input onChange={(e)=>setFullName(e.target.value)} value={fullName}
            type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required />
           )}
@@ -41,6 +52,20 @@ const LoginPage = () => {
           <button type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
             {currState==="Sign up"?"Create Account":"Login Now"}
           </button>
+
+          <div className='flex items-center gap-2 text-sm text-gray-500'>
+            <input type="checkbox" />
+            <p> Agree to the terms of use & privacy policy.</p>
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            {currState === "Sign up"? (
+              <p className='text-sm text-gray-600'>Already have an account? <span onClick={()=>{setCurrState("Login");setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+            ):(
+              <p className='text-sm text-gray-600'>Create an account<span onClick={()=>setCurrState("Sign up")} className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
+            )}
+
+          </div>
         
       </form>
 
