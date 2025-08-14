1acf92c554c48b65bd383a72a1aa81cca899eb63
Saloni <salonikhandelwal22553@gmail.com>
2025-08-14 08:13:53 +0530

    feat(server): add message controller, routes, models, and Cloudinary integration

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
@@ -50,4 +51,28 @@ export const login = async (req,res)=> {
 //Controller to check if user is authenticated
 export const checkAuth=(req,res)=>{
     res.json({success:true,user:req.user});
+}
+
+//Controller to update user profile details
+export const updateProfile=async(req,res)=>{
+    try{
+        const{profilePic,bio,fullName}=req.body;
+        const userId=req.user._id;
+        let updateUser;
+        if(!profilePic){
+            updateUser=await User.findByIdAndUpdate(userId,{bio,fullName},{new:true});
+
+        }
+        else{
+            const upload=await cloudinary.uploader.upload(profilePic);
+            updateProfile=await User.findByIdAndUpdate(userId,{profilePic:upload.secure_url,bio,fullName},{new:true});
+        }
+        res.json({success:true,user:updateUser})
+
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+
+    }
 }
--- server/server.js
+++ server/server.js
@@ -3,16 +3,47 @@ import 'dotenv/config';
 import cors from 'cors';
 import http from 'http';
 import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+import {Server} from "socket.io";
 
 //Create Express app and HTTP server
 const app=express();
 const server=http.createServer(app)
 
+//Initialise socket.io server
+export const io=new Server(server,{
+    cors:{origin:"*"}
+})
+
+//Store online users
+export const userSocketMap={};//{userId:socketId}
+
+//Socket.io connection handler
+io.on ("connection",(socket)=>{
+    const userId=socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId) userSocketMap[userId]=socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+    socket.io("discoonect",()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+})
+
 //Middleware setup
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
 
+//Routes  setup
 app.use("/api/status",(req,res)=>res.send("Server is live"));
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter)
 
 //Connect to mongodb
 await connectDB();
